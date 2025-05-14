b68a03acee9edde7b771b39dd623b59f891c9358
Alejandro Ramos <alejandrorrua@gmail.com>
2025-05-13 21:37:16 -0500

    actualización de inicio

--- app/(general)/inicio/page.tsx
+++ app/(general)/inicio/page.tsx
@@ -19,14 +19,7 @@ export const metadata: Metadata = {
 
 export default function HomePage() {
   return (
-<<<<<<< HEAD
-    <section>
-      <div>
-        
-      </div>
-    </section>
-  )
-=======
+
     <div className="mr-[20px] ml-[20px] sm:mr-[20px] sm:ml-[20px] md:mr-[40px] md:ml-[40px] lg:mr-[80px] lg:ml-[80px]">
       <SeccionBody
         img={"https://www.rabimport.com/image/icon-asesores.png"}
@@ -70,5 +63,5 @@ export default function HomePage() {
 
     </div>
   );
->>>>>>> 9dc8c3076c238deb325aedf976902be44ab89d7c
+
 }
